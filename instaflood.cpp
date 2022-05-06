c3e207b71530479a8dbd47c8561bef8f67b218cc
matpech <47293035+matFPV@users.noreply.github.com>
2022-05-06 09:29:22 +0200

    Add files via upload

---
+++ instaflood.cpp
@@ -0,0 +1,25 @@
+//this is the non delay version for flooding as fast as posible not regarding app delays or limitations
+
+#include <iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<string>
+using namespace std;
+//vars
+int loop;
+int repeatfor;
+//main
+int main() {
+	std::cout << "--- use at your own risk ---" << endl;
+		//repfor
+	std::cout << "repeat for:";
+	std::cin >> repeatfor;
+		//time to click into a nother window
+	system("sleep 5");
+		//loop
+		while (loop<repeatfor){
+		system("xdotool type \"<put your text here>\"");
+		system("xdotool key KP_Enter");
+		loop++;
+	}
+}
